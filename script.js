5ca656b4d774685596e7cee6e5939ef0e5a4aedd
AH-2005 <ctw1725@gmail.com>
2026-02-13 22:02:06 +0000

    First js draft

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const activeCounter = 0
+const completeCounter = 0
 
+
+document.addEventListener("DOMContentLoaded", function addTask() => {
+document.querySelector("#submit").onclick = () => {
+Const activeTask = document.createElement("li").innerHTML = document.querySelector("div").value 
+document.querySelector("#activeList").append(activeTask)
+
+document.createElement("button")
+document.querySelector("activeTask").append("button")
+
+Document.querySelector("taskField").value = ""
+activeCounter++
+}
+}) 
