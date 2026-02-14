130719923904078fe3fc5898c4fb47e9dfaa4628
AH-2005 <ctw1725@gmail.com>
2026-02-14 19:06:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const activeCounter = 0
-const completeCounter = 0
-
-
 document.addEventListener("DOMContentLoaded", function addTask() => {
-document.querySelector("#submit").onclick = () => {
-Const activeTask = document.createElement("li").innerHTML = document.querySelector("div").value 
-document.querySelector("#activeList").append(activeTask)
+//When the submit button is clicked:
+document.querySelector("[value='Add Task']").onclick =
+() =>
 
-document.createElement("button")
-document.querySelector("activeTask").append("button")
-
-Document.querySelector("taskField").value = ""
+const taskId = Math.floor(Math.random() * 10000) 
+const taskCard = {}
+const activeCounter = 0 
+const completeCounter = 0
+  
+taskCard.name = document.querySelector("#taskField").value
+taskCard.id = taskId
+taskCard.priorityLevel = document.querySelector("#priorityLevel").value 
+taskCard.description = document.querySelector("#description").value
+taskCard.date = document.querySelector("#dateField").value 
+document.createElement("li").value = taskCard 
+document.querySelector("#activeList").append("li")  
+activeTasks.push(taskCard)
 activeCounter++
-}
-}) 
+document.querySelector("#activeCounter").innerHTML = activeCounter
+})
+
