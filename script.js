195e30f98c141bcc33471a67d080eb99b560297a
AH-2005 <ctw1725@gmail.com>
2026-02-23 20:05:21 +0000

    Add conditional statement that only enables submit button if task title is not empty

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 document.querySelector("#addTask").disabled = true;
+  document.querySelector("#titleField").onkeyup = function () {
+    if (!document.querySelector("#titleField").value.length > 0) {
+      document.querySelector("#addTask").disabled = true;
+    } else {
+      document.querySelector("#addTask").disabled = false;
+    }
+  }
 })
