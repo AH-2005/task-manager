7ef57b102c14f0d34b54d3a8c8da449a6631ef3b
AH-2005 <ctw1725@gmail.com>
2026-02-24 20:44:37 +0000

    Store task details in an object + display task name when submit button is clicked

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ document.querySelector("#addTask").disabled = true;
       document.querySelector("#addTask").disabled = false;
     }
   }
+  
+let activeTasks = [];
+let activeCounter = 0;
+
+
+  document.querySelector("#addTask").onclick = function addTask () {
+    
+  let activeTask = {}; 
+    activeTask.name = querySelector("#titleField").value;
+    activeTask.description = querySelector("#description").value;
+    activeTask.date = querySelector("#dateField").value;
+    activeTask.priority = querySelector("#priorityLevel").value;
+
+    let li = document.createElement("li")
+    li.innerHTML = activeTask.name;
+  }
 })
